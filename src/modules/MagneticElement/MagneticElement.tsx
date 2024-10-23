d9c10a5d5dde1d27c56aa512e4771cd22eed8ebf
daramosn <daramosn@unal.edu.co>
2024-10-22 20:48:49 -0500

    feat: add Magnetic element demo

---
+++ src/modules/MagneticElement/MagneticElement.tsx
@@ -0,0 +1,123 @@
+import { motion } from 'framer-motion'
+import { useRef, useState, type ReactNode, type SVGProps } from 'react'
+
+import './MagneticElement.css'
+
+const Magnetic = ({ children }: { children: ReactNode }) => {
+  const refMagnetic = useRef<HTMLDivElement | null>(null)
+
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const mouseMoveHandler = (e: React.MouseEvent) => {
+    const { clientX, clientY } = e
+    if (!refMagnetic.current) return
+    const { top, left, width, height } = refMagnetic.current.getBoundingClientRect()
+    const x = clientX - (left + width / 2)
+    const y = clientY - (top + height / 2)
+
+    setPosition({ x, y })
+  }
+  const mouseLeaveHandler = () => setPosition({ x: 0, y: 0 })
+
+  return (
+    <motion.div
+      ref={refMagnetic}
+      onMouseMove={mouseMoveHandler}
+      onMouseLeave={mouseLeaveHandler}
+      animate={{ x: position.x, y: position.y }}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+const MagneticElement = () => {
+  return (
+    <div className="magnetic-elements">
+      <h1>Follow for more!</h1>
+      <div className="container">
+        <Magnetic>
+          <InstagramIcon width="155px" height="155px"></InstagramIcon>
+        </Magnetic>
+        <Magnetic>
+          <IconYoutube width="170px" height="170px"></IconYoutube>
+        </Magnetic>
+        <Magnetic>
+          <TwitchIcon width="160px" height="160px"></TwitchIcon>
+        </Magnetic>
+        <Magnetic>
+          <TwitterIcon width="150px" height="150px"></TwitterIcon>
+        </Magnetic>
+      </div>
+    </div>
+  )
+}
+
+export default MagneticElement
+
+function IconYoutube(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73"
+      ></path>
+    </svg>
+  )
+}
+
+function TwitchIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43Z"
+      ></path>
+    </svg>
+  )
+}
+
+function InstagramIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"
+      ></path>
+    </svg>
+  )
+}
+
+function TwitterIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z"
+      ></path>
+    </svg>
+  )
+}

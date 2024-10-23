0731d9e57b59e8f6585511831b0aa556f2ce1eb2
daramosn <daramosn@unal.edu.co>
2024-10-22 20:47:10 -0500

    dep: add React integration and Framer motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
-import vue from '@astrojs/vue'
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), react()],
   vite: {
     optimizeDeps: {
       include: ['src/components/*.vue', 'gsap']
     }
   }
-})
+});

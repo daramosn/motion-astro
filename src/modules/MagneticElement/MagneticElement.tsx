f10448b2ca5537c99436e4e0e92d323f1a727bc4
daramosn <daramosn@unal.edu.co>
2025-01-19 18:15:27 -0500

    dep: update Motion dep and remove framer-motion (deprecated)

--- src/modules/MagneticElement/MagneticElement.tsx
+++ src/modules/MagneticElement/MagneticElement.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { useRef, useState, type ReactNode, type SVGProps } from 'react'
 
 import './MagneticElement.css'

052ec6f9c53c6309bddec58bbf35dc3da40db98f
daramosn <daramosn@unal.edu.co>
2025-03-02 22:06:23 -0500

    config: disable astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +10,8 @@ export default defineConfig({
     optimizeDeps: {
       include: ['src/components/*.vue', 'gsap']
     }
+  },
+  devToolbar: {
+    enabled: false
   }
-});
+})

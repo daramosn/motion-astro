7c9474faa005a4e61da26f23af7b83e66cb15ea2
daramosn <daramosn@unal.edu.co>
2025-08-07 19:48:04 -0500

    chore: refactor image imports to use dynamic glob loading

--- src/modules/ImageTrail/ImageTrail.vue
+++ src/modules/ImageTrail/ImageTrail.vue
@@ -102,19 +102,13 @@
 import { animate, type AnimationSequence } from 'motion'
 import { ref, useTemplateRef } from 'vue'
 
-import img1 from './images/1.webp'
-import img2 from './images/2.webp'
-import img3 from './images/3.webp'
-import img4 from './images/4.webp'
-import img5 from './images/5.webp'
-import img6 from './images/6.webp'
-import img7 from './images/7.webp'
-import img8 from './images/8.webp'
-import img9 from './images/9.webp'
-import img10 from './images/10.webp'
-import img11 from './images/11.webp'
+const images = Object.values(
+  import.meta.glob('./images/*.webp', {
+    eager: true,
+    import: 'default'
+  })
+)
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11]
 const buttons = [1, 2, 3, 4]
 
 const currentSequence = ref(1)
